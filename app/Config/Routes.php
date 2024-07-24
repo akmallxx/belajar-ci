2ce84249f2fd4793d83cd15140c95c34c829359d
khanifr <khanifrokhawi@gmail.com>
2024-07-24 15:37:17 +0700

    rekap harian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,28 +16,36 @@ $routes->post('login_action', 'User::login_action');
 // Admin
 $routes->get('admin/home', 'Admin\Home::index', ['filter' => 'AdminFilter']);
 
-$routes->get('admin/jabatan', 'Admin\Jabatan::index', ['filter' => 'AdminFilter']); 
-$routes->get('admin/jabatan/create', 'Admin\Jabatan::create', ['filter' => 'AdminFilter']); 
-$routes->post('admin/jabatan/store', 'Admin\Jabatan::store', ['filter' => 'AdminFilter']); 
-$routes->get('admin/jabatan/edit/(:segment)', 'Admin\Jabatan::edit/$1', ['filter' => 'AdminFilter']); 
+$routes->get('admin/jabatan', 'Admin\Jabatan::index', ['filter' => 'AdminFilter']);
+$routes->get('admin/jabatan/create', 'Admin\Jabatan::create', ['filter' => 'AdminFilter']);
+$routes->post('admin/jabatan/store', 'Admin\Jabatan::store', ['filter' => 'AdminFilter']);
+$routes->get('admin/jabatan/edit/(:segment)', 'Admin\Jabatan::edit/$1', ['filter' => 'AdminFilter']);
 $routes->post('admin/jabatan/update/(:segment)', 'Admin\Jabatan::update/$1', ['filter' => 'AdminFilter']);
-$routes->get('admin/jabatan/delete/(:segment)', 'Admin\Jabatan::delete/$1', ['filter' => 'AdminFilter']); 
+$routes->get('admin/jabatan/delete/(:segment)', 'Admin\Jabatan::delete/$1', ['filter' => 'AdminFilter']);
 
-$routes->get('admin/lokasi_presensi', 'Admin\LokasiPresensi::index', ['filter' => 'AdminFilter']); 
-$routes->get('admin/lokasi_presensi/create', 'Admin\LokasiPresensi::create', ['filter' => 'AdminFilter']); 
-$routes->post('admin/lokasi_presensi/store', 'Admin\LokasiPresensi::store', ['filter' => 'AdminFilter']); 
-$routes->get('admin/lokasi_presensi/edit/(:segment)', 'Admin\LokasiPresensi::edit/$1', ['filter' => 'AdminFilter']); 
+$routes->get('admin/lokasi_presensi', 'Admin\LokasiPresensi::index', ['filter' => 'AdminFilter']);
+$routes->get('admin/lokasi_presensi/create', 'Admin\LokasiPresensi::create', ['filter' => 'AdminFilter']);
+$routes->post('admin/lokasi_presensi/store', 'Admin\LokasiPresensi::store', ['filter' => 'AdminFilter']);
+$routes->get('admin/lokasi_presensi/edit/(:segment)', 'Admin\LokasiPresensi::edit/$1', ['filter' => 'AdminFilter']);
 $routes->post('admin/lokasi_presensi/update/(:segment)', 'Admin\LokasiPresensi::update/$1', ['filter' => 'AdminFilter']);
-$routes->get('admin/lokasi_presensi/delete/(:segment)', 'Admin\LokasiPresensi::delete/$1', ['filter' => 'AdminFilter']); 
-$routes->get('admin/lokasi_presensi/detail/(:segment)', 'Admin\LokasiPresensi::detail/$1', ['filter' => 'AdminFilter']); 
+$routes->get('admin/lokasi_presensi/delete/(:segment)', 'Admin\LokasiPresensi::delete/$1', ['filter' => 'AdminFilter']);
+$routes->get('admin/lokasi_presensi/detail/(:segment)', 'Admin\LokasiPresensi::detail/$1', ['filter' => 'AdminFilter']);
 
-$routes->get('admin/data_pegawai', 'Admin\DataPegawai::index', ['filter' => 'AdminFilter']); 
-$routes->get('admin/data_pegawai/create', 'Admin\DataPegawai::create', ['filter' => 'AdminFilter']); 
-$routes->post('admin/data_pegawai/store', 'Admin\DataPegawai::store', ['filter' => 'AdminFilter']); 
-$routes->get('admin/data_pegawai/edit/(:segment)', 'Admin\DataPegawai::edit/$1', ['filter' => 'AdminFilter']); 
+$routes->get('admin/data_pegawai', 'Admin\DataPegawai::index', ['filter' => 'AdminFilter']);
+$routes->get('admin/data_pegawai/create', 'Admin\DataPegawai::create', ['filter' => 'AdminFilter']);
+$routes->post('admin/data_pegawai/store', 'Admin\DataPegawai::store', ['filter' => 'AdminFilter']);
+$routes->get('admin/data_pegawai/edit/(:segment)', 'Admin\DataPegawai::edit/$1', ['filter' => 'AdminFilter']);
 $routes->post('admin/data_pegawai/update/(:segment)', 'Admin\DataPegawai::update/$1', ['filter' => 'AdminFilter']);
-$routes->get('admin/data_pegawai/delete/(:segment)', 'Admin\DataPegawai::delete/$1', ['filter' => 'AdminFilter']); 
-$routes->get('admin/data_pegawai/detail/(:segment)', 'Admin\DataPegawai::detail/$1', ['filter' => 'AdminFilter']); 
+$routes->get('admin/data_pegawai/delete/(:segment)', 'Admin\DataPegawai::delete/$1', ['filter' => 'AdminFilter']);
+$routes->get('admin/data_pegawai/detail/(:segment)', 'Admin\DataPegawai::detail/$1', ['filter' => 'AdminFilter']);
+
+$routes->get('admin/rekap_harian', 'Admin\RekapHarian::index', ['filter' => 'AdminFilter']);
+$routes->get('admin/rekap_harian/create', 'Admin\RekapHarian::create', ['filter' => 'AdminFilter']);
+$routes->post('admin/rekap_harian/store', 'Admin\RekapHarian::store', ['filter' => 'AdminFilter']);
+$routes->get('admin/rekap_harian/edit/(:segment)', 'Admin\RekapHarian::edit/$1', ['filter' => 'AdminFilter']);
+$routes->post('admin/rekap_harian/update/(:segment)', 'Admin\RekapHarian::update/$1', ['filter' => 'AdminFilter']);
+$routes->get('admin/rekap_harian/delete/(:segment)', 'Admin\RekapHarian::delete/$1', ['filter' => 'AdminFilter']);
+$routes->get('admin/rekap_harian/detail/(:segment)', 'Admin\RekapHarian::detail/$1', ['filter' => 'AdminFilter']);
 
 
 // Pegawai
---
+++ app/Views/admin/rekap_harian/edit.php
@@ -0,0 +1,37 @@
+<?= $this->extend('admin/layout.php'); ?>
+<?= $this->section('content'); ?>
+
+<div class="card col-md-6">
+    <div class="card-body">
+        <form method="post" action="<?= base_url('admin/rekap_harian/update/' . $rekap_harian['id']) ?>">
+
+            <div class="input-style-1">
+                <label>Nama Pegawai</label>
+                <input type="text" class="form-control" value="<?= htmlspecialchars($rekap_harian['nama']) ?>" readonly />
+                <input type="hidden" name="id_pegawai" value="<?= $rekap_harian['id_pegawai'] ?>" />
+            </div>
+
+            <div class="input-style-1">
+                <label>Tanggal Masuk</label>
+                <input type="date" class="form-control" name="tanggal_masuk" value="<?= $rekap_harian['tanggal_masuk'] ?>" required />
+            </div>
+            <div class="input-style-1">
+                <label>Jam Masuk</label>
+                <input type="time" class="form-control" name="jam_masuk" value="<?= $rekap_harian['jam_masuk'] ?>" required />
+            </div>
+            <div class="input-style-1">
+                <label>Tanggal Keluar</label>
+                <input type="date" class="form-control" name="tanggal_keluar" value="<?= $rekap_harian['tanggal_keluar'] ?>" required />
+            </div>
+            <div class="input-style-1">
+                <label>Jam Keluar</label>
+                <input type="time" class="form-control" name="jam_keluar" value="<?= $rekap_harian['jam_keluar'] ?>" required />
+            </div>
+            <div class="input-style-1">
+                <button type="submit" class="btn btn-primary">Update</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+<?= $this->endSection(); ?>
