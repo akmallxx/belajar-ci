cbcb6df5174349897de7e63f1417b150f4b9cb2f
rizalporto12 <volcanoriasli11@gmail.com>
2024-07-25 09:41:01 +0700

    penyesuaian

--- app/Views/login.php
+++ app/Views/login.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="shortcut icon" href="<?= base_url('assets/images/logo/loco.svg'); ?>" type="image/x-icon" />
-    <title>Sign In | PlainAdmin Demo</title>
+    <title>Sign In | AbsensiKu</title>
 
     <!-- ========== All CSS files linkup ========= -->
     <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>" />
