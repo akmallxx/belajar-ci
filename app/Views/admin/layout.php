9d8b4a90df8f310a6b0ae5bb7d577bfd5adafc72
rizalporto12 <volcanoriasli11@gmail.com>
2024-07-25 13:04:01 +0700

    penyesuaian2

--- app/Views/admin/layout.php
+++ app/Views/admin/layout.php
@@ -204,8 +204,8 @@
             <div class="copyright text-center text-md-start">
               <p class="text-sm">
                 Designed and Developed by
-                <a href="https://plainadmin.com" rel="nofollow" target="_blank">
-                  PlainAdmin
+                <a href="" rel="nofollow" target="_blank" disabled>
+                  AbsensiKu
                 </a>
               </p>
             </div>
@@ -213,8 +213,8 @@
           <!-- end col-->
           <div class="col-md-6">
             <div class="terms d-flex justify-content-center justify-content-md-end">
-              <a href="#0" class="text-sm">Term & Conditions</a>
-              <a href="#0" class="text-sm ml-15">Privacy & Policy</a>
+              <!-- <a href="#0" class="text-sm">Term & Conditions</a>
+              <a href="#0" class="text-sm ml-15">Privacy & Policy</a> -->
             </div>
           </div>
         </div>
